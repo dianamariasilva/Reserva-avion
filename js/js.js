ec920ab841a37c70014aa8446aba9bbb4c76c335
dianamariasilva <diana.silva.v94@gmail.com>
2017-09-27 06:08:20 -0500

    estilo a reserva de aviones

--- js/js.js
+++ js/js.js
@@ -9,15 +9,15 @@ $(document).ready(function() {
   var currentCell;
   function redirect(event){
   console.log(event);
-      var asiento = (event.target.textContent);
-      console.log(asiento);
-      $("#asiento").val(asiento);
-      // var mostrar = $("#mostrar");
-      // mostrar.html(asiento);
+      var seat = (event.target.textContent);
+      console.log(seat);
+      $("#seat").val(seat);
+      // var show = $("#show");
+      // show.html(seat);
   
       currentCell = event.target;
   
-      var num = parseInt (mostrar.textContent);
+      var num = parseInt (show.textContent);
       var obj = arraySeat[num-1];
       if (obj !== undefined) {
           $("#name").val(obj.name);
@@ -25,13 +25,13 @@ $(document).ready(function() {
           $("#dni").val(obj.dni);
       }
   }
-  $("#reservar").click(reservar);
-  function reservar(){
-    console.log("reservar");
+  $("#reserve").click(reserve);
+  function reserve(){
+    console.log("reserve");
     console.log(arraySeat);
-      var numeroAsiento = parseInt ($('#asiento').val());
-     if (numeroAsiento != '')
-      arraySeat[numeroAsiento-1] = {
+      var numeroseat = parseInt ($('#seat').val());
+     if (numeroseat != '')
+      arraySeat[numeroseat-1] = {
           name: $("#name").val(),
           lastname: $("#lastname").val(),
           dni: $("#dni").val(),
@@ -48,10 +48,10 @@ $(document).ready(function() {
       $("#dni").val("");
      }
   
-  $("#cancelar").click(cancelar);
-   function cancelar(){
-     var num = parseInt (mostrar.textContent);
-     var numeroAsiento = parseInt (mostrar.textContent);
+  $("#cancel").click(cancel);
+   function cancel(){
+     var num = parseInt (show.textContent);
+     var numeroseat = parseInt (show.textContent);
      var obj = arraySeat[num-1];
      if (obj !== undefined) {
            $("#name").val("");
@@ -62,8 +62,8 @@ $(document).ready(function() {
       }
    }
 
-   $("#listar").click(listar);
-   function listar(){
+   $("#toList").click(toList);
+   function toList(){
     var html = "";
     for (var i =0; i < arraySeat.length; i++) {
       var dato_1 = arraySeat[i];
@@ -76,22 +76,22 @@ $(document).ready(function() {
       html += s;
     }
    }
-     $('#mostrar2').html(html);
+     $('#show2').html(html);
   }
   
-  function buscar(){
+  function search(){
     var html = "";
-    var buscarDni = $("#dni").val();
+    var searchDni = $("#dni").val();
     for(var i=1; i<arraySeat.length; i++){
       var dato_1 = arraySeat[i-1];
-      if(arraySeat[i-1]!= undefined && buscarDni == dato_1.dni){
+      if(arraySeat[i-1]!= undefined && searchDni == dato_1.dni){
     var s = "<div>" + 
       "<h1>Nombre: " +  dato_1.name + "</h1>" + "<h2> Apellido:" + dato_1.lastname +"</h2>" + "<h2>DNI: " + dato_1.dni +"</h2>"+
       "</div>";
       html += s;
       }
     }
-      $('#mostrar3').html(html);
+      $('#show3').html(html);
   }
   
   
